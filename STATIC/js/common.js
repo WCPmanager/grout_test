eb477c3dd12c696cf0831ae01bb30cd32535c3a0
gobukfarmer <92699551+gobukfarmer@users.noreply.github.com>
2024-09-09 00:04:22 +0900

    작업파일 - JAVA

---
+++ STATIC/js/common.js
@@ -0,0 +1,17 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', vh + 'px');
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', vh + 'px');
+});
+
+function siblingsRemove(target, className){
+    const children = target.parentElement.children;
+    for (let i = 0; i < children.length; i++) {    
+        children[i].classList.remove('active');
+    } 
+    target.classList.add(className); 
+}
+function getElementIndex(element) {
+  return [].indexOf.call(element.parentNode.children, element);
+}
